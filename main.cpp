7ba5eb0d46b0d6bda089f1e674f270b03320982e
Emmy Veselinov <emmyveselinov@gmail.com>
2022-04-07 21:23:28 -0400

    added task B

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ Classes
 #include "time.h"
 
 int main(){
-
+    //task A
     Time t1A = {10, 32};
     int result1A = minutesSinceMidnight(t1A);
     std::cout << "==============TASK A==============" << std::endl;
@@ -17,9 +17,16 @@ int main(){
 
     Time t2Aa = {10, 30};
     Time t2Ab = {13, 40};
-
     int result1B = minutesUntil(t2Aa, t2Ab);
-    std::cout << "==============TASK B==============" << std::endl;
     std::cout << "Number of minutes in between " << t2Aa.h << ":" << t2Aa.m << " and " << t2Ab.h << ":" << t2Ab.m << " is " << result1B << " MINUTES\n" << std::endl;
+
+    //task B
+    Time time0B = {8,10};
+    int min1 = 75;
+    Time result2A = addMinutes(time0B, min1);
+    std::cout << "==============TASK B==============" << std::endl;
+    std::cout << "Old Time: " << time0B.h << ":" << time0B.m << " Minutes Shift: " << min1 << " New Time: " << result2A.h << ":" << result2A.m << std::endl;
+
+    
     return 0;
 }
