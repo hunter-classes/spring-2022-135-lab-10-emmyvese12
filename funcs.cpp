27af5e771b37a2eaec08b53d02ffd279f8b544e3
Emmy Veselinov <emmyveselinov@gmail.com>
2022-04-07 23:16:33 -0400

    added functions for task C

--- funcs.cpp
+++ funcs.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 #include "funcs.h"
 #include "time.h"
+#include "movie.h"
+#include "timeslot.h"
 
 //task A
 int minutesSinceMidnight(Time time){
@@ -44,4 +47,39 @@ Time addMinutes(Time time0, int min){
     Time result = {time0.h, time0.m};
 
     return result;
+}
+
+//task C
+//to_string used to convert an int to a string
+std::string printMovie(Movie mv){
+    std::string g;
+    std::string output = "";
+    switch (mv.genre) {
+        case ACTION   : g = "ACTION"; break;
+        case COMEDY   : g = "COMEDY"; break;
+        case DRAMA    : g = "DRAMA";  break;
+        case ROMANCE  : g = "ROMANCE"; break;
+        case THRILLER : g = "THRILLER"; break;
+    }
+    output = mv.title + " " + g + " (" + std::to_string(mv.duration) + " min)";
+
+    return output;
+}
+
+std::string getTimeSlot(TimeSlot ts){
+    std::string result = "";
+    result += printMovie(ts.movie);
+    result += " [starts at ";
+    result += std::to_string(ts.startTime.h) + ":" + std::to_string(ts.startTime.m);
+    result += ",";
+    result += " ends by "; //ending time is the startime + duration
+
+    Time t1 = {ts.startTime.h, ts.startTime.m};
+    Time endTime = addMinutes(t1, ts.movie.duration);
+    result += std::to_string(endTime.h) + ":" + std::to_string(endTime.m);
+    result += "]";
+
+    return result;
+
+
 }
